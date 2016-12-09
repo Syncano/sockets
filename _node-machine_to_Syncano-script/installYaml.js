9eae086ad7a5fb937b61290bb016de7f55c428f9
Tomasz Ciecierski <tomek.ciecierski@syncano.com>
2016-12-09 21:32:05 +0100

    Add not fully working zip installation

--- _node-machine_to_Syncano-script/installYaml.js
+++ _node-machine_to_Syncano-script/installYaml.js
@@ -1,38 +1,34 @@
 const { request, BLACK_LIST, rawAddress } = require('./helpers');
 const fs = require('fs');
+const newAccountInfo = require('./newAccountInfo');
+const { apiKey, instanceName } = newAccountInfo;
+const FormData = require('form-data');
 const validSocketsFile = './valid_sockets.json';
 const machines = require('./machines.json');
-const bluebird = require('bluebird');
 const zipFolder = require('zip-folder');
+const json = {};
+const archiver = require('archiver');
 
-
-function removeValidSocketsFile() {
-  try {
-    fs.accessSync(validSocketsFile);
-    fs.unlinkSync(validSocketsFile);
-  } catch (e) {}
-}
 
 function getSocket(name) {
   return request
     .get(`/${name}/`)
+    // .then(() => console.log('getSocketRequest'))
     .then((response) => {
-      const json = {};
       const status = response.data.status;
       json[name] = status;
-      fs.appendFileSync(validSocketsFile, JSON.stringify(json, null, 2));
-      return response;
+      fs.writeFileSync(validSocketsFile, JSON.stringify(json, null, 2));
+      return name;
     })
-    .catch(console.log);  
+    .then(() => name)
+
+    // .catch(console.log);
 };
 
 function deleteSocket(name) {
   return request
     .delete(`/${name}/`)
     .then((res) => {
-      const json = {};
-      json[name] = 'Deleted';
-      fs.appendFileSync(validSocketsFile, JSON.stringify(json, null, 2));
       return res.url;
     })
     .catch(console.log);
@@ -42,20 +38,9 @@ function whiteListMachine(machine, blackList) {
   return blackList.indexOf(machine.identity) === -1;
 }
 
-function installSocket(name, install_url) {
-  return request
-    .post('/install/',
-      {
-        name,
-        install_url
-      }
-    )
-    .then(() => name);
-};
-
 function checkIfInstalled(name) {
-  return getSocket(name).then((response) => {
-    console.log('res', response.data.status);
+  getSocket(name).then((response) => {
+    // console.log(name,`:`, response.data.status);
 
     return new Promise((resolve, reject) => {
       if (response.data.status === 'ok') {
@@ -67,30 +52,81 @@ function checkIfInstalled(name) {
       }, 3000);
     });
   })
+  .then(() => deleteZip(path))
   .catch(checkIfInstalled);
+}
+
+function createZip(name, version) {
+  return new Promise((resolve, reject) => {
+    const output = fs.createWriteStream(`../${name}-${version}.zip`, { mode: 0o700 });
+    const archive = archiver('zip', {
+      store: true
+    });
+    archive.pipe(output);
+    archive.on('error', reject);
+
+    archive.file(`./socket.yml`);
+    archive.directory(`../${name}/${version}/scripts`, 'scripts');
+    archive.finalize();
+
+    output.on('close', resolve);
+  });
+}
+
+// function createZip(name, version) {
+//   zipFolder(`../${name}/${version}`,`../${name}-${version}.zip`, function(err) {
+//     if(err) {
+//       console.log('oh no!', err);
+//     } else {
+//       console.log(name, 'created');
+//     }
+//   });
+// }
+
+function deleteZip(name) {
+  fs.unlinkSync(name);
+  console.log('deleted', name)
+}
+
+function installSocket(socketName, path) {
+  const endpointPath = `/v2/instances/${instanceName}/sockets/`;
+  return new Promise((resolve, reject) => {
+    const form = new FormData();
+    form.append('name', socketName);
+    form.append('zip_file', fs.createReadStream(path));
+
+    form.submit({
+      method: 'POST',
+      protocol: 'https:',
+      host: 'api.syncano.rocks',
+      headers: {
+        'X-Api-Key': apiKey
+      },
+      path: endpointPath
+
+    }, (err, res) => {
+      if (err || res.statusCode === 404) {
+        reject(err || res);
+      }
+        // fs.unlink(`${this.projectPath}/socket.zip`);
+        resolve(res);
+      });
+  });
 }
 
 const promises = machines
   .filter((machine) => whiteListMachine(machine, BLACK_LIST))
   .map((machine, index) => {
     const { variableName, version } = machine;
+    const path = `../${variableName}-${version}.zip`;
+    createZip(variableName, version);
 
-    zipFolder(`../${variableName}/${version}/`, `../${variableName}-${version}.zip`, function(err) {
-      if(err) {
-          console.log('oh no!', err);
-      } else {
-         console.log('EXCELLENT');
-      }
-    });
-    const install_url = `${rawAddress}/${variableName}/${version}/socket.yaml`;
-
-    const zip_file = require(`../${variableName}-${version}.zip`);
-    // console.log('zipFile', zip_file);
-    return () => installSocket(variableName, install_url)
-      .then(checkIfInstalled)
-      .then(deleteSocket)
-      .then(() => console.log('done'))
-      .catch((err) => console.log(index, err.response.status))
+    return () => installSocket(variableName, path)
+      .then(() => checkIfInstalled(variableName))
+      .then(() => deleteSocket(variableName))
+      .then(() => console.log('Done'))
+      // .then(() => deleteZip(path))
+            // .catch((err) => console.log(index, err))
 });
 
 const runPromisesInSequence = (p, fn) => p.then(fn);
